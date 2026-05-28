37b64ddd789173fea43762b7b5030ced3995dd70
honzikec <honza.kaiser@gmail.com>
2026-05-29 01:24:52 +0200

    betterstack

--- app/layout.tsx
+++ app/layout.tsx
@@ -24,6 +24,22 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
+      <head>
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `
+              !function(b,e,t,r){
+                b[t]=b[t]||function(...args){(b[t].q=b[t].q||[]).push(args)};
+                b[t].l=+new Date;
+                var s=e.createElement('script'); s.async=1; s.crossOrigin='anonymous';
+                s.src='https://betterstack.net/b.js?t='+r;
+                (e.head||e.getElementsByTagName('head')[0]).appendChild(s);
+              }(window,document,'betterstack','A2q9aFSoHHvEzAjShz6b5unQ');
+              betterstack('init', { environment: 'production' });
+            `,
+          }}
+        />
+      </head>
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
